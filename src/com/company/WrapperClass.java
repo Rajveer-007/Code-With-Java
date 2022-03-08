01677053f187edd308ee5329eede30bea3de1567
Rajveer <www.rajveer2103@gmaiil.com>
2022-03-09 01:16:20 +0530

    Added Files

---
+++ src/com/company/WrapperClass.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class WrapperClass {
+    public static void main(String[] args) {
+        // Unboxing
+        Integer a = new Integer(3);
+        int i = a.intValue();
+        int j = a;
+        System.out.println("a = " +a+ " i = " +i+ " j = " +j);
+
+        // autoboxing
+        int x = 25;
+        Integer y = Integer.valueOf(x);
+        Integer z = y;
+        System.out.println("x = " +x+ " y = " +y+ " z = " +z);
+    }
+}
